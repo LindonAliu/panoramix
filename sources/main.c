a54c657068c90c8e13fef8f7f9c57cbf3d3655b7
Lindon ALIU <lindon.aliu@epitech.eu>
2023-04-25 01:02:40 +0200

    fix: for the begenning of project, add just return 84 in main.

--- sources/main.c
+++ sources/main.c
@@ -5,18 +5,9 @@
 ** FreeKOSOVO
 */
 
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
-
 #include "all_lib.h"
 
 int main(UNUSED int argc, UNUSED const char *argv[], UNUSED const char *env[])
 {
-
-    int a = 0;
-    char *phrase = "10";
-
-    a = my_getnbr(phrase);
-    printf("%d\n", a);
+    return 84;
 }
