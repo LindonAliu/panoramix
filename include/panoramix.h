82b989bb8bfad28d699b2768fde419e659a54338
Lindon ALIU <lindon.aliu@epitech.eu>
2023-05-02 23:24:20 +0200

    feat: add a new architecture of project

--- include/panoramix.h
+++ include/panoramix.h
@@ -9,9 +9,30 @@
     #define MY_PROJECT_H_
 
     #include "all_lib.h"
+    #include <pthread.h>
+    #include <stdbool.h>
 
 void print_help(const char *bin_name);
 int error_handling(int argc, const char *argv[]);
 int panoramix(const char *av[]);
+
+struct villager {
+    size_t id;
+    size_t nb_fights;
+    bool drunk;
+    struct village *village;
+};
+
+struct druid {
+    size_t pot_size;
+    size_t nb_refills;
+    struct village *village;
+};
+
+struct village {
+    size_t nb_potions;
+    bool call_druid;
+    pthread_mutex_t potion_mutex;
+};
 
 #endif/* !MY_PROJECT_H_ */
